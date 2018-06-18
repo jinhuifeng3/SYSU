749dec416cb667f56fb99f4fa1475a8955e92044
jinhuifeng <huifengking@hotmail.com>
2018-06-18 20:59:35 +0800

    hw9

---
+++ Unity3d-learning/Unity3D-HW9/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;  
+using System.Collections;  
+using Vuforia;  
+using System;  
+  
+public class NewBehaviourScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    GameObject player;
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        player.
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        
+    }
+
+    // Use this for initialization  
+
+    void Start()
+    {
+        m1 = transform.GetChild(0).GetComponent<MeshRenderer>().material;
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            vbs[i].RegisterEventHandler(this);//把ImageTarget下所有含有VirtualButtonBehaviour组件的物体注册过来（使用前面写的Pressed和Released方法处理）。  
+        }
+
+    }
+
+}
